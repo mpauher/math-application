23bfc120abd53b26f6200968109299a64dae7b29
Jairo <jairojairo40@gmail.com>
2025-11-16 21:43:07 -0500

    Little changes

--- lib/home.dart
+++ lib/home.dart
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Organizador de Turnos'),
+        title: const Text('Organizador y optimizador'),
       ),
       body: Center(
         child: Padding(
